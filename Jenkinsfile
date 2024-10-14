d99c9dcf2db26b9e93f961e679127d626bdd3391
PA20003 <pa20003@ues.edu.sv>
2024-10-14 13:00:29 -0600

    testing #1 y jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,10 @@
+pipeline {
+    agent any
+    stages {
+        stage ('Hola mundo pipeline'){
+            steps {
+                echo 'Hello owrld!'
+            }
+        }
+    }
+}
---
+++ test/1holamundo.js
@@ -0,0 +1,7 @@
+import { assert } from 'chai';
+
+describe('01 prueba hola mundo', () => {
+    it('estoy haciendo la primera prueba', () => {
+        assert.equal(true, true);
+    });
+});
